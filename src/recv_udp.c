09ae3aaddd436d6dff4e441d094186eddaf3230f
Yuval Saraf <yuvalsaraf@gmail.com>
2015-09-04 10:29:25 -0700

    noy is princess

--- src/recv_udp.c
+++ src/recv_udp.c
@@ -9,26 +9,29 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/*
+
 void printsin(struct sockaddr_in *s, char *str1, char *str2) {
+
   printf("%s\n", str1);
-  printf("%s: ", str2);
-  -- port: sin->sin_port (host integer type) 
-  -- IP: sin->sin_addr (IP in dotted-decimal notation) 
+  printf("%s ", str2);
+  printf("ip= %s , port= %d", inet_ntoa(s->sin_addr), s->sin_port);
   printf("\n");
 }
-*/
+
 
 int main(int argc, char *argv[])
 {
+  // Define varibales
   int socket_fd, cc, fsize;
   struct sockaddr_in  s_in, from;
   struct { char head; u_long  body; char tail;} msg;
 
+  // Create socket for communication
   socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
 
   bzero((char *) &s_in, sizeof(s_in));  /* They say you must do this    */
 
+  // Define the address used by the socket
   s_in.sin_family = (short)AF_INET;
   s_in.sin_addr.s_addr = htonl(INADDR_ANY);    /* WILDCARD */
   s_in.sin_port = htons((u_short)0x3333);
@@ -36,12 +39,18 @@ int main(int argc, char *argv[])
   //printsin( &s_in, "RECV_UDP", "Local socket is:"); 
   fflush(stdout);
 
+  // Binds the unbound socket to the address given
   bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in));
 
+  // Loops infinitly
   for(;;) {
     fsize = sizeof(from);
+
+    // Receives a message and store it into msg varibale
     cc = recvfrom(socket_fd,&msg,sizeof(msg),0,(struct sockaddr *)&from,&fsize);
-    //printsin( &from, "recv_udp: ", "Packet from:");
+    printsin( &from, "recv_udp: ", "Packet from:");
+
+    // Prints the message
     printf("Got data ::%c%ld%c\n",msg.head,(long) ntohl(msg.body),msg.tail); 
     fflush(stdout);
   }
--- src/send_udp.c
+++ src/send_udp.c
@@ -9,22 +9,30 @@
 
 int main(int argc, char *argv[])
 {
+  // Define varibales
   int socket_fd;
   struct sockaddr_in  dest;
   struct hostent *hostptr;
   struct { char head; u_long body; char tail; } msgbuf;
 
+  // Create socket for communication
   socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
   bzero((char *) &dest, sizeof(dest)); /* They say you must do this */
+
+  // Get the host by the host name 
   hostptr = gethostbyname(argv[1]);
+
+  // Define the address used by the socket
   dest.sin_family = (short) AF_INET;
   bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
   dest.sin_port = htons((u_short)0x3333);
 
+  // Create the message
   msgbuf.head = '<';
   msgbuf.body = htonl(getpid()); /* IMPORTANT! */
   msgbuf.tail = '>';
 
+  // Send the message to the socket 
   sendto(socket_fd,&msgbuf,sizeof(msgbuf),0,(struct sockaddr *)&dest,
                   sizeof(dest));
 
