20d038b61b13cbaa04a63792fff39d4e50f26a31
Yuval Saraf <yuvalsaraf@gmail.com>
2015-09-05 18:18:54 +0300

    Finished first part of B

--- src/net_client.c
+++ src/net_client.c
@@ -8,20 +8,46 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #define SIM_LENGTH 10 
-#define IP_ADDRESS "?????" 
 #define PORT 1337 
 
-int main(void)
+int main(int argc, char* argv[])
 { 
+  // Defien variables
+  struct addrinfo* res;
+  char* hostname;
+  char* hostaddr;
+  struct sockaddr_in* saddr;
   int sock; 
   struct sockaddr_in cli_name; 
   int count;
   int value; 
 
+  // Check if hostname was sent
+  if (argc != 2) {
+    perror("Usage: net_client.o <hostname>\n");
+    exit(1);
+  }
+
+  // Get hostname
+  hostname = argv[1];
+  
+  // Get address by hostname string
+  if (0 != getaddrinfo(hostname, NULL, NULL, &res)) {
+    fprintf(stderr, "Error in resolving hostname %s\n", hostname);
+    exit(1);
+  }
+
+  // Resolve host address as string
+  saddr = (struct sockaddr_in*)res->ai_addr;
+  hostaddr = inet_ntoa(saddr->sin_addr);
+
   printf("Client is alive and establishing socket connection.\n");
   
   
+  // Create a socket of stream type
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0)
     { perror ("Error opening channel");
@@ -30,12 +56,14 @@ int main(void)
     }
       
 
+  // Define properties required to connect to the socket
   bzero(&cli_name, sizeof(cli_name)); 
   cli_name.sin_family = AF_INET; 
-  cli_name.sin_addr.s_addr = inet_addr(IP_ADDRESS); 
+  cli_name.sin_addr.s_addr = inet_addr(hostaddr); 
   cli_name.sin_port = htons(PORT);
 
 
+  // Connect to the socket, if return 0 then succeeded, if not, print error
   if (connect(sock, (struct sockaddr *)&cli_name, sizeof(cli_name)) < 0)
     { perror ("Error establishing communications");
       close(sock);
@@ -43,13 +71,16 @@ int main(void)
     }
 
 
+  // Loop 10 times
   for (count = 1; count <= SIM_LENGTH; count++)
-    { read(sock, &value, 4);
+    { 
+      // Get values from the socket into value variable
+      read(sock, &value, 4);
       printf("Client has received %d from socket.\n", value);
     }
 
   printf("Exiting now.\n");
-
+  // Close the scoekt
   close(sock); 
   exit(0); 
 
--- src/net_server.c
+++ src/net_server.c
@@ -10,6 +10,11 @@
 #define SIM_LENGTH 10 
 #define PORT 1337 
 
+
+/**
+  This method is used by the server to clean up before existing, 
+  only prints and closes the socket 
+*/
 void clean_up(int cond, int *sock)
 { printf("Exiting now.\n");
   close(*sock); 
@@ -18,39 +23,50 @@ void clean_up(int cond, int *sock)
 
 int main(void)
 { 
+  // Define the variables
   int sock; 
   int connect_sock; 
   struct sockaddr_in serv_name; 
-  size_t len; 
+  int len; 
   int count;
   
+  // Create a socket of stream type
   sock = socket(AF_INET, SOCK_STREAM, 0);  
   if (sock < 0)
     { perror ("Error opening channel");
       clean_up(1, &sock);
     }
   
-  bzero(&serv_name, sizeof(serv_name)); 
+  bzero(&serv_name, sizeof(serv_name));
+
+  // Define socket's properties so the socket could listen 
   serv_name.sin_family = AF_INET;                                   
   serv_name.sin_port = htons(PORT); 
 
+  // Bind that socket to the address given
   if (bind(sock, (struct sockaddr *)&serv_name, sizeof(serv_name)) < 0)
     { perror ("Error naming channel");
       clean_up(1, &sock);
     }
       
   printf("Server is alive and waiting for socket connection from client.\n");
+  // Listen to the socket and accept messages
   listen(sock, 1); 
-
   len = sizeof(serv_name);
+  // Get a socket to send responses to
   connect_sock = accept(sock, (struct sockaddr *)&serv_name, &len);
 
+  // Loop 10 times
   for (count = 1; count <= SIM_LENGTH; count++)
-    { write(connect_sock, &count, 4);
+    { 
+      // Write a response on the connected socket
+      write(connect_sock, &count, 4);
       printf("Server has written %d to socket.\n", count);
     }
 
+  // Close the connected socket
   close(connect_sock); 
+  // Perform a clean up before termination
   clean_up(0, &sock); 
 
 }
